1c5577788c9501f125d929f344482a5cebb9071d
Chris Brown <cryobrown@gmail.com>
2017-03-08 13:57:36 -0600

    pre-demo work

--- src/level.cpp
+++ src/level.cpp
@@ -15,6 +15,7 @@ const char* const level_names[] = {
         "../assets/level/more_platforms.json",
         "../assets/level/shafts.json",
         "../assets/level/close_quarters.json",
+        //"../assets/level/complex.json"
 };
 
 const double line_width = 4;
